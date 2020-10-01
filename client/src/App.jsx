b78d64d5307a497c3cdd84505226712587a2ae7e
rqsell <68514875+rqsell@users.noreply.github.com>
2020-10-01 18:07:35 -0400

    stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import Home from './components/Home';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Home />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" render={() => <Home />} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
