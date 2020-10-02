3ef4370fefd61001e5af7bb19b0f3d44ac4d79ff
Rabiul Alam <rabiul.alam@outlook.com>
2020-10-02 12:16:13 -0400

    Movie detail

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Movies =  require('../models/Movie.model');
+const Movies = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res) => {
-  console.log('called');
-  res.json({ message: 'index' });
+router.get("/", (req, res) => {
+  console.log("called");
+  res.json({ message: "index" });
 });
 
-router.get('/movies', (req, res) => {
-  console.log('called');
+router.get("/movies", (req, res) => {
+  console.log("called");
   Movies.find().then((movies) => {
-    console.log(movies)
-    res.json({movies});
-  })
+    console.log(movies);
+    res.json({ movies });
+  });
 });
 
+//get movie details
+router.get("/movies/:id", (req, res) => {
+  console.log(req.params.id);
+  Movies.findById(req.params.id).then((movie) => {
+    res.json({ movie });
+  });
+});
 module.exports = router;
